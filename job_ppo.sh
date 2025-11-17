6da42522fb0ae60876f88e87ea6d40f38932c405
moskitoo <tomek_niedzi@o2.pl>
2025-11-17 10:21:03 +0100

    added bash jobs to run on the hpc - to be configured

---
+++ job_ppo.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+### General options
+### –- specify queue --
+#BSUB -q hpc
+### -- set the job Name --
+#BSUB -J quadruped_rl_ppo_4096
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+### -- Set the span of the job to 1 node --
+#BSUB -R "span[hosts=1]"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 4:00
+# request system-memory
+#BSUB -R "rusage[mem=8GB]"
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o job_logs/quadruped_rl_ppo_%J.out
+#BSUB -e quadruped_rl_%J.err
+# -- end of LSF options --
+
+source /zhome/d4/a/214319/miniconda3/bin/activate
+
+conda activate quadruped
+
+python run_sb3_wandb.py --project-name quadruped_rl  --use-gpu --save-path /work3/s243600 --learning-alg PPO --num-envs 4096
