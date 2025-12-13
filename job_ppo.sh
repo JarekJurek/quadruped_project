6130ea385a79284723288c66bf4e543328cbfa80
moskitoo <tomek_niedzi@o2.pl>
2025-12-13 13:26:55 +0100

    added new line with script call

--- job_ppo.sh
+++ job_ppo.sh
@@ -3,13 +3,13 @@
 ### –- specify queue --
 #BSUB -q hpc
 ### -- set the job Name --
-#BSUB -J quadruped_rl_ppo_fixed_1_1_finetune
+#BSUB -J quadruped_rl_ppo_old_reward_wo_drift
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 30
 ### -- Set the span of the job to 1 node --
 #BSUB -R "span[hosts=1]"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 1:45
+#BSUB -W 5:00
 # request system-memory
 #BSUB -R "rusage[mem=4GB]"
 #BSUB -R "select[model==XeonGold6226R]"
@@ -33,4 +33,6 @@ conda activate quadruped
 
 # python run_sb3_wandb.py --project-name quadruped_rl --run-name obstacles_curr_scratch_wo_weights --save-path /work3/s243600 --learning-alg PPO --num-envs 40 --task_env FWD_CUSTOM --observation_space_mode LR_COURSE_OBS_EXTENDED --total_timesteps 5000000 --learning_rate 3e-4 --max_episode_length 20 --learning_rate_adaptive --num_stairs 5 --terrain_difficulty 3 --terrain STAIRS --difficulty_objective height --drift_weight 0.0 --yaw_weight 0.0 --orientation_weight 0.0 --height_weight 0.0 --survival_weight 0.0
 
-python run_sb3_wandb.py --project-name quadruped_rl --run-name fixed_1_1_finetune --save-path /work3/s243600 --learning-alg PPO --num-envs 40 --task_env FWD_CUSTOM --observation_space_mode LR_COURSE_OBS_EXTENDED --total_timesteps 3000000 --learning_rate 3e-4 --max_episode_length 20 --learning_rate_adaptive --des_x_vel 1.1 --drift_weight 0.0 --yaw_weight 0.0 --orientation_weight 0.0 --height_weight 0.0 --survival_weight 0.0 --load-nn --load_model_path /work3/s243600/logs/intermediate_models/quadruped_rl/simple_fixed_0_8/120925154658
+# python run_sb3_wandb.py --project-name quadruped_rl --run-name fixed_0_8_kp_80 --save-path /work3/s243600 --learning-alg PPO --num-envs 40 --task_env FWD_CUSTOM --observation_space_mode LR_COURSE_OBS_EXTENDED --total_timesteps 5000000 --learning_rate 3e-4 --max_episode_length 20 --learning_rate_adaptive --des_x_vel 0.6 --drift_weight 0.0 --yaw_weight 0.0 --orientation_weight 0.0 --height_weight 0.0 --survival_weight 0.0 --kp 80 --load-nn --load_model_path /work3/s243600/logs/intermediate_models/quadruped_rl/simple_fixed_0_8/120925154658
+
+python run_sb3_wandb.py --project-name quadruped_rl --run-name old_reward_wo_drift --save-path /work3/s243600 --learning-alg PPO --num-envs 40 --task_env FWD_CUSTOM_OLD --observation_space_mode LR_COURSE_OBS_EXTENDED --total_timesteps 5000000 --learning_rate 3e-4 --max_episode_length 20 --learning_rate_adaptive --des_x_vel 0.6 --disable_energy True --disable_orientation True --disable_yaw True --disable_drift True
